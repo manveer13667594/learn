2a7bc3a4cbfdc19b2634d02eb85a431d8d13a1b4
Manveer Singh <manveer13667594@gmail.com>
2025-08-01 17:28:04 +0530

    Added navigation drawer containing colors and alert dialog option

--- app/src/main/java/com/example/learn/topics/alertDialogBox.kt
+++ app/src/main/java/com/example/learn/topics/alertDialogBox.kt
@@ -1,14 +1,7 @@
-import android.graphics.drawable.Icon
-import android.net.wifi.hotspot2.pps.HomeSp
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ColorScheme
-import androidx.compose.material3.Icon
-import androidx.compose.material3.SegmentedButtonDefaults.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -17,11 +10,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.key.Key.Companion.Home
 import androidx.compose.ui.unit.dp
 import com.example.learn.topics.ColorBox
-import com.example.learn.ui.theme.PrimaryDarkColor
 
 @Composable
 fun AlertDialogBox() {
--- app/src/main/java/com/example/learn/topics/topicColors.kt
+++ app/src/main/java/com/example/learn/topics/topicColors.kt
@@ -1,5 +1,4 @@
 package com.example.learn.topics
-
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
